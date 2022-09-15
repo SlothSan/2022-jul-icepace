51b8a52e96ecac3861f1c99d181ab8d7c9efca16
Mike Oram <Mikeoram01@gmail.com>
2022-09-15 20:41:38 +0100

    refactored loginPage, works but needs styling and for the buttons to be linked up

--- logInUser.php
+++ logInUser.php
@@ -1,11 +1,35 @@
 <?php
 require_once 'vendor/autoload.php';
-
+session_start();
 use Icepace\LoginUser;
-use Icepace\UserCreator;
+if(isset($_POST['usernameInput']) && isset($_POST['passwordInput'])) {
+    if($_POST['usernameInput'] == '' || $_POST['passwordInput'] == '') {
+        header('Location:login.php');
+    } else {
+        $username = $_POST['usernameInput'];
+        $password = $_POST['passwordInput'];
+        $db = new PDO('mysql:host=db; dbname=icepace', 'root', 'password');
+        $resultArray = (LoginUser::checkingUserExists($db, $username));
+        if(password_verify($password, $resultArray['hashed_pass'])) {
+            $_SESSION['loggedIn'] = true;
+        } else {
+            echo "Your password was incorrect!";
+        }
+    }
 
-$username = $_POST['usernameInput'];
-$db = new PDO('mysql:host=db; dbname=icepace', 'root', 'password');
-$resultArray = (LoginUser::checkingUserExists($db, $username));
-print_r($resultArray);
+} else {
+    header('Location:login.php');
+}
+?>
+<html>
+<body>
+<? if($_SESSION['loggedIn'] === true) {
+    $userProfileString = 'userProfile.php?username=' . $username;
+    echo '<p>You have logged in sucessfully</p>';
+    echo '<a href="' . $userProfileString . '"/>Go to profile</a>';
 
+} else {
+    echo "<p>Your password is incorrect </p>";
+} ?>
+</body>
+</html>

f9ee09057c6840d42f67ca60c9495273d80a8436
Mike Oram <Mikeoram01@gmail.com>
2022-09-15 18:40:41 +0100

    Removed the login.scss and merge into index.scss

--- login.php
+++ login.php
@@ -16,7 +16,6 @@ print_r($_POST);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="index.css"/>
-    <link rel="stylesheet" type="text/css" href="login.css" />
     <title>Icepace</title>
 </head>
 <body>

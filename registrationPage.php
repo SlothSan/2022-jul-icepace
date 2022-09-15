fdd93f1b28daec789de5455f41f4d4a82e47682b
Mike Oram <Mikeoram01@gmail.com>
2022-09-15 17:43:26 +0100

    Refactored SCSS to all be in index.scss to stop users having to watch multiple files to create the needed .css files

--- registrationPage.php
+++ registrationPage.php
@@ -21,7 +21,6 @@ if(isset($_SESSION['errors'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="index.css"/>
-    <link rel="stylesheet" type="text/css" href="registrationPage.css"/>
     <title>Icepace</title>
 </head>
 <body>
